888f88d0ddc0467fd5bb0be584eebaa76fcff532
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-26 02:06:48 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -46,6 +46,8 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
+  total = 0 
+  
   
   pp source
 end
