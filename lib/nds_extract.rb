7f7fad53573dc18cccd2459c942aa7a65187f5e1
Bill Griffin <williamgriffin81@hotmail.com>
2019-10-26 02:32:51 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -46,19 +46,19 @@ def total_gross(source)
   # Visit each key (i.e. director name), look up the value in the hash
   # returned by directors_totals, and add it to a running total. When done,
   # return the total
-  total = 0 
-  index = 0 
-  
-  dir_earnings = directors_totals(source)
+  dir_to_earnings_hash = directors_totals(source)
   dir_names = list_of_directors(source)
-  
-  while index < dir_names.length do 
-    dir_name = dir_names[index]
-    total += dir_earnings(dir_name)
-    index += 1 
+  i = 0
+
+  total = 0
+
+  while i < dir_names.length do
+    dir_name = dir_names[i]
+    total += dir_to_earnings_hash[dir_name]
+    i += 1
   end
-  
-  total 
+
+  total
 end
 
 
